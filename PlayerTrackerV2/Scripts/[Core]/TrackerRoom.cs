820a1da720be2a0b87933266090abc7915c8a964
kuzuriyager <134836877+KuzuriYager@users.noreply.github.com>
2023-10-08 08:52:32 -0400

    Rework Update +Patch

---
+++ PlayerTrackerV2/Scripts/[Core]/TrackerRoom.cs
@@ -0,0 +1,31 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+namespace Lastation.PlayerTrackerV2
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class TrackerRoom : UdonSharpBehaviour
+    {
+        public string roomName;
+        public Transform[] probes;
+        public int playersInRoom;
+
+        public float RoomProbeCheck(VRCPlayerApi _player)
+        {
+            float smallestDistance = float.PositiveInfinity;
+            for (int i = 0; i < probes.Length; i++)
+            {
+                if (!probes[i]) continue;
+
+                float distance = Vector3.Distance(_player.GetPosition(), probes[i].position);
+                
+                if (distance < smallestDistance)
+                    smallestDistance = distance;
+            }
+            return smallestDistance;
+        }
+    }
+
+}
